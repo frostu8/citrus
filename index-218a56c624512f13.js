72d454ee47ae1a2bf6caa56c1a7e4e2d710fb947
frostu8 <frostu8@users.noreply.github.com>
2021-04-15 22:15:34 +0000

    deploy: bd4265dde97f059d96251653f049cd0646b00f89

--- index-218a56c624512f13.js
+++ index-218a56c624512f13.js
@@ -317,7 +317,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-fd71861e93ece4d0_bg.wasm', import.meta.url);
+        input = new URL('index-218a56c624512f13_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
